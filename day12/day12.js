10e766989349bc592071946ae19727665e646e4b
Solubaf <alhomme6@gmail.com>
2022-11-28 10:48:29 +0100

    init

---
+++ day12/day12.js
@@ -0,0 +1,23 @@
+import * as fs from 'fs';
+
+const timer = (script, input) => {
+    var start = performance.now();
+    script(input);
+    var end = performance.now();
+    return (end - start).toFixed(2);
+};
+
+const partOne = (input) => {
+    
+};
+
+const partTwo = (input) => {
+
+};
+//, 'puzzle.in'
+['example.in'].forEach((file) => {
+    const input = fs.readFileSync(`day1/${file}`, 'utf-8').trim().split('');
+    //console.log(input);
+    console.log(`Result of part one for ${file} : ` + partOne(input) + ` (executed in ${timer(partOne, input)} ms)`);
+    console.log(`Result of part two for ${file} : ` + partTwobis(input) + ` (executed in ${timer(partTwo, input)} ms)`);
+});
