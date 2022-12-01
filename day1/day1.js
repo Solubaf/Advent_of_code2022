39826b7c54c5a2e71b49b115e922a3c0662ebe8f
Solubaf <alhomme6@gmail.com>
2022-12-01 09:03:46 +0100

    setup+day1

--- day1/day1.js
+++ day1/day1.js
@@ -8,16 +8,35 @@ const timer = (script, input) => {
 };
 
 const partOne = (input) => {
+
+return input.reduce((acc,cur)=>{
+    const parse=cur.split('\r\n').map(Number);
+    //console.log(parse);
+    const sum=parse.reduce((acc2,cur2)=>{return acc2+cur2},0);
+    return Math.max(sum,acc);
+},0);
     
 };
 
 const partTwo = (input) => {
+    const top3=[];
+    let sum=input.map(cur=>{const parse=cur.split('\r\n').map(Number);
+        //console.log(parse);
+        return parse.reduce((acc2,cur2)=>{return acc2+cur2},0);});
+        //console.log(sum);
+    for(var i=0;i<3;i++){
+        top3.push(sum.reduce((cur,acc)=>Math.max(cur,acc),0));
+        //console.log(top3);
+        sum[sum.indexOf(top3[i])]=0;
+    }
+    //console.log(top3);
 
+    return top3.reduce((cur,acc)=>acc+cur,0);
 };
-//, 'puzzle.in'
-['example.in'].forEach((file) => {
-    const input = fs.readFileSync(`day1/${file}`, 'utf-8').trim().split('');
+//
+['example.in', 'puzzle.in'].forEach((file) => {
+    const input = fs.readFileSync(`day1/${file}`, 'utf-8').trim().split('\r\n\r\n');
     //console.log(input);
     console.log(`Result of part one for ${file} : ` + partOne(input) + ` (executed in ${timer(partOne, input)} ms)`);
-    console.log(`Result of part two for ${file} : ` + partTwobis(input) + ` (executed in ${timer(partTwo, input)} ms)`);
+    console.log(`Result of part two for ${file} : ` + partTwo(input) + ` (executed in ${timer(partTwo, input)} ms)`);
 });
