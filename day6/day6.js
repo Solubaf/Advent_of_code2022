5a72a0575c7980076724bc85db3b48831cf6cd0d
Solubaf <alhomme6@gmail.com>
2022-12-06 09:14:36 +0100

    day 6 working

--- day6/day6.js
+++ day6/day6.js
@@ -8,16 +8,47 @@ const timer = (script, input) => {
 };
 
 const partOne = (input) => {
+    let index=0;
+    let tab=[];
+    while(tab.length<4&&index<input.length){
+        tab=[];
+        tab.push(input[index]);
+        for(var i=1;i<4;i++){
+            if(!tab.includes(input[index+i])){
+                tab.push(input[index+i]);
+            }else{
+                break;
+            }
+        }
+        index++;
+        
+    }
+    return index+3;
     
 };
 
 const partTwo = (input) => {
-
+        let index=0;
+        let tab=[];
+        while(tab.length<14&&index<input.length){
+            tab=[];
+            tab.push(input[index]);
+            for(var i=1;i<14;i++){
+                if(!tab.includes(input[index+i])){
+                    tab.push(input[index+i]);
+                }else{
+                    break;
+                }
+            }
+            index++;
+            
+        }
+        return index+13;
 };
 //, 'puzzle.in'
-['example.in'].forEach((file) => {
-    const input = fs.readFileSync(`day6/${file}`, 'utf-8').trim().split('');
-    console.log(input);
+['example.in', 'puzzle.in'].forEach((file) => {
+    const input = fs.readFileSync(`day6/${file}`, 'utf-8').trim();
+    //console.log(input);
     console.log(`Result of part one for ${file} : ` + partOne(input) + ` (executed in ${timer(partOne, input)} ms)`);
-    // console.log(`Result of part two for ${file} : ` + partTwo(input) + ` (executed in ${timer(partTwo, input)} ms)`);
+    console.log(`Result of part two for ${file} : ` + partTwo(input) + ` (executed in ${timer(partTwo, input)} ms)`);
 });
